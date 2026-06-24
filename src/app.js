7d16e49edc9cc026de7c116c75e71f3a971d0903
Ayush16110 <newayushgupta2017@gmail.com>
2026-06-24 23:57:04 +0530

    feat : System Health Check Added

---
+++ src/app.js
@@ -0,0 +1,34 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+// Basic configurations
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: "true", limit: "16kb" }));
+app.use(express.static("public"));
+
+// CORS configurations
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    }),
+);
+
+// Import routes
+import healthCheckRouter from "./routes/healthcheck.routes.js";
+
+app.use("/api/v1/healthcheck", healthCheckRouter);
+
+app.get("/", (req, res) => {
+    res.send("Welcome to the Project Management Platform!");
+});
+
+app.get("/instagram", (req, res) => {
+    res.send("This is a Instagram Page...");
+});
+
+export default app;
